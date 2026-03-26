eb0913c831dec7ce964c83aef9e50dc47c719976
evisagovmd <evisa.gov2020.md@gmail.com>
2026-03-26 14:11:22 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+function checkVisa(){
+
+var visa=document.getElementById("visa").value.trim();
+var passport=document.getElementById("passport").value.trim();
+var robot=document.getElementById("robot").checked;
+
+if(!robot){
+alert("Please confirm you are not a robot");
+return;
+}
+
+fetch("visas.json")
+.then(response => response.json())
+.then(data => {
+
+var records=data.records;
+
+var found=false;
+
+for(var i=0;i<records.length;i++){
+
+if(records[i].visa_number==visa && records[i].passport==passport){
+
+showResult(records[i]);
+
+found=true;
+
+break;
+
+}
+
+}
+
+if(!found){
+
+document.getElementById("result").innerHTML=
+"<div class='result-error'>No visa record found</div>";
+
+}
+
+});
+
+}
+
+
+
+function showResult(record){
+
+var table=`
+
+<table class="result-table">
+
+<tr>
+<th>Name</th>
+<td>${record.name}</td>
+</tr>
+
+<tr>
+<th>Passport Number</th>
+<td>${record.passport}</td>
+</tr>
+
+<tr>
+<th>Nationality</th>
+<td>${record.nationality}</td>
+</tr>
+
+<tr>
+<th>Visa Type</th>
+<td>${record.visa_type}</td>
+</tr>
+
+<tr>
+<th>Status</th>
+<td class="status">${record.status}</td>
+</tr>
+
+</table>
+
+`;
+
+document.getElementById("result").innerHTML=table;
+
+}
