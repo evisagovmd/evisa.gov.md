ad9249a5955345e714fc9b670e2e4a24ae321df4
evisagovmd <evisa.gov2020.md@gmail.com>
2026-04-04 17:23:02 +0545

    Create router.js

---
+++ router.js
@@ -0,0 +1,13 @@
+const routes = {
+"/check-visa": "/check-my-visa.html",
+"/apply": "/apply-now.html",
+"/visa-requirements": "/do-i-need-a-visa.html",
+"/application-status": "/check-your-application-status.html",
+"/continue-application": "/continue-application.html"
+};
+
+const path = window.location.pathname;
+
+if(routes[path]){
+window.location.replace(routes[path]);
+}
