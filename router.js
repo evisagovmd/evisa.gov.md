1d11358851d7eca275c49e5fc7aa39fde894a17d
evisagovmd <evisa.gov2020.md@gmail.com>
2026-07-19 00:12:10 +0545

    Update router.js

--- router.js
+++ router.js
@@ -1,13 +1,50 @@
+(function(){
+
 const routes = {
-"/check-visa": "/check-my-visa.html",
-"/apply": "/apply-visa.html",
-"/visa-requirements": "/do-i-need-a-visa.html",
-"/application-status": "/check-your-application-status.html",
-"/continue-application": "/continue-application.html"
+
+"check-visa":
+"check-my-visa.html",
+
+"apply":
+"apply-visa.html",
+
+"visa-requirements":
+"do-i-need-a-visa.html",
+
+"application-status":
+"check-your-application-status.html",
+
+"continue-application":
+"continue-application.html"
+
 };
 
-const path = window.location.pathname;
+const pathParts =
+window.location.pathname
+.split("/")
+.filter(Boolean);
 
-if(routes[path]){
-window.location.replace(routes[path]);
+const lastPart =
+pathParts[
+pathParts.length - 1
+] || "";
+
+if(!routes[lastPart]){
+return;
 }
+
+const baseParts =
+pathParts.slice(0,-1);
+
+const target =
+"/" +
+(
+baseParts.length
+? baseParts.join("/") + "/"
+: ""
+) +
+routes[lastPart];
+
+window.location.replace(target);
+
+})();
